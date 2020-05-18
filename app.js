37c116a6fb709944c6635dd8c19220fa9c8ff1be
Qasim Wani <qasim31wani@gmail.com>
2020-05-18 04:22:24 -0400

    wildcard on CORB

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(require("express-session")({
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://vti-api.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET');
